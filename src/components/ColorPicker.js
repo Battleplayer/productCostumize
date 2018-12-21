f4265ef8ea1bdadeedf22b720c00dcf9d52ed535
unknown <battleplayerx@gmail.com>
2018-12-21 18:06:09 +0200

    'img_work'

--- src/components/ColorPicker.js
+++ src/components/ColorPicker.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React from 'react';
 import {
     Accordion,
     AccordionItem,
@@ -10,107 +10,117 @@ import 'react-accessible-accordion/dist/fancy-example.css';
 import './ColorPicker.css';
 
 
-const colors = ['#ff0000', '#00ff00', '#0000ff', '#ff8800', '#eeff00'];
+const colors = ['#382E2C','#545876', '#53565A', '#C1A0DA', '#857874', '#A51890','#CA005D', '#C4D600', '#007681', '#0df', ''];
 
-// class ColorPicker extends Component {
-//
-//     render() {
 export default (props) => {
-        const {onChange, onClick, TieColor, onImageSet, getScale, getRotate, isImported} = props;
-        // console.log(this.props);
-        return (
-            <div style={{flex: 1}}>
-                <h3>Design your tie</h3>
-                <Accordion>
-                    <AccordionItem>
-                        <AccordionItemTitle>
-                            <h3>Control pattern</h3>
-                        </AccordionItemTitle>
-                        <AccordionItemBody>
-                            <div>
-                                <input type="range" name="scale"
-                                       min="0" max="1" defaultValue={0.5} step='0.1'
-                                       onChange={getScale}/>
-                                <label htmlFor="scale">Scale</label>
-                            </div>
-                            <div>
-                                <input type="range" name="Rotate"
-                                       min="0" max="360" defaultValue={180} step='18'
-                                       onChange={getRotate}/>
-                                <label htmlFor="Rotate">Rotate</label>
-                            </div>
-                            <div>
-                                <input type="range" name="Horizontal"
-                                       min="0" max="100" defaultValue={50} step='10'/>
-                                <label htmlFor="Horizontal">Horizontal</label>
-                            </div>
-                            <div>
-                                <input type="range" name="Vertical"
-                                       min="0" max="50" defaultValue={25} step='7'/>
-                                <label htmlFor="Vertical">Vertical</label>
-                            </div>
-                        </AccordionItemBody>
-                    </AccordionItem>
-                    <AccordionItem>
-                        <AccordionItemTitle>
-                            <h3>Apply Base Color</h3>
-                            <div>Some colors</div>
-                        </AccordionItemTitle>
-                        <AccordionItemBody>
-                            <ul className='colorlist'>
-                                {colors.map(color => (
-                                    <li key={color}>
-                                        <span style={{backgroundColor: color}} onClick={onClick}> </span>
-                                    </li>
-                                ))}
-                            </ul>
-                            <p>
-                                <input type="color" value={TieColor} onChange={onChange}/>
-                            </p>
-                        </AccordionItemBody>
-                    </AccordionItem>
-                    <AccordionItem>
-                        <AccordionItemTitle>
-                            <h3>Tie textures</h3>
-                            <p>empty</p>
-                        </AccordionItemTitle>
-                        <AccordionItemBody>
-                            <div>enter something</div>
-                        </AccordionItemBody>
-                    </AccordionItem>
-                    <AccordionItem>
-                        <AccordionItemTitle>
-                            <h3>Upload your image</h3>
-                        </AccordionItemTitle>
-                        <AccordionItemBody>
-                            <p><input type="file" accept="image/*" onChange={onImageSet}/></p>
-                            {isImported ?
-                                <div>
-                                    FINISH WITH PIC
-                                    <div>
-                                        <input type="range" name="Rotate"
-                                               min="0" max="360" defaultValue={180} step='18'
-                                               onChange={getRotate}/>
-                                        <label htmlFor="Rotate">Rotate</label>
-                                    </div>
-                                    <div>
-                                        <input type="range" name="Horizontal"
-                                               min="0" max="100" defaultValue={50} step='10'/>
-                                        <label htmlFor="Horizontal">Horizontal</label>
-                                    </div>
-                                    <div>
-                                        <input type="range" name="Vertical"
-                                               min="0" max="50" defaultValue={25} step='7'/>
-                                        <label htmlFor="Vertical">Vertical</label>
-                                    </div>
-                                </div> : ''}
-                        </AccordionItemBody>
-                    </AccordionItem>
-                </Accordion>
+    const {
+        onChange, onClick, TieColor, onImageSet, getScale, getRotate, isImported,
+        getTieY, getTieX,
+        imgX, imgY, imgRotate, imgScale
+    } = props;
+    // console.log(this.props);
+    return (
+        <div >
+            <div id="image-wrapper">
+            <h3>Design your tie</h3>
+            <Accordion>
+                <AccordionItem>
+                    <AccordionItemTitle>
+                        <h3>Control pattern</h3>
+                    </AccordionItemTitle>
+                    <AccordionItemBody>
+                        <div>
+                            <label htmlFor="scale">Scale</label>
+                            <input type="range" name="scale"
+                                   min="0" max="3" defaultValue={1} step='0.2'
+                                   onChange={getScale}/>
+                        </div>
+                        <div>
+                            <label htmlFor="Rotate">Rotate</label>
+                            <input type="range" name="Rotate"
+                                   min="-180" max="180" defaultValue={0} step='1'
+                                   onChange={getRotate}/>
+                        </div>
+                        <div>
+                            <label htmlFor="Horizontal">Horizontal</label>
+                            <input type="range" name="Horizontal"
+                                   min="0" max="70" defaultValue={30} step='10'
+                                   onChange={getTieX}/>
 
+                        </div>
+                        <div>
+                            <label htmlFor="Vertical">Vertical</label>
+                            <input type="range" name="Vertical"
+                                   min="0" max="100" defaultValue={50} step='7'
+                                   onChange={getTieY}/>
+                        </div>
+                    </AccordionItemBody>
+                </AccordionItem>
+                <AccordionItem>
+                    <AccordionItemTitle>
+                        <h3>Apply Base Color</h3>
+                        <div>Some colors</div>
+                    </AccordionItemTitle>
+                    <AccordionItemBody>
+                        <ul className='colorlist'>
+                            {colors.map(color => (
+                                <li key={color}>
+                                    <span style={{backgroundColor: color}} onClick={onClick}> </span>
+                                </li>
+                            ))}
+                        </ul>
+                        <p>
+                            <input type="color" value={TieColor} onChange={onChange}/>
+                        </p>
+                    </AccordionItemBody>
+                </AccordionItem>
+                <AccordionItem>
+                    <AccordionItemTitle>
+                        <h3>Tie textures</h3>
+                        <p>empty</p>
+                    </AccordionItemTitle>
+                    <AccordionItemBody>
+                        <div>enter something</div>
+                    </AccordionItemBody>
+                </AccordionItem>
+                <AccordionItem>
+                    <AccordionItemTitle>
+                        <h3>Upload your image</h3>
+                    </AccordionItemTitle>
+                    <AccordionItemBody>
+                        <p><input type="file" accept="image/*" onChange={onImageSet}/></p>
+                        {isImported ?
+                            <div>
+                                <div>
+                                    <label htmlFor="ScaleImg">Scale</label>
+                                    <input type="range" name="ScaleImg"
+                                           min="0" max="2" defaultValue={1} step='0.1'
+                                           onChange={imgScale}/>
+                                </div>
+                                <div>
+                                    <label htmlFor="RotateImg">Rotate</label>
+                                    <input type="range" name="RotateImg"
+                                           min="0" max="360" defaultValue={180} step='18'
+                                           onChange={imgRotate}/>
+                                </div>
+                                <div>
+                                    <label htmlFor="HorizontalImg">Horizontal</label>
+                                    <input type="range" name="HorizontalImg"
+                                           min="-100" max="700" defaultValue={280} step='20'
+                                           onChange={imgX}/>
+                                </div>
+                                <div>
+                                    <label htmlFor="VerticalImg">Vertical</label>
+                                    <input type="range" name="VerticalImg"
+                                           min="0" max="1400" defaultValue={1000} step='75'
+                                           onChange={imgY}/>
+                                </div>
+                            </div> : ''}
+                    </AccordionItemBody>
+                </AccordionItem>
+            </Accordion>
             </div>
-        )
-    // }
+        </div>
+    )
 }
 
-// export default ColorPicker;
--- src/container/MainContainer.js
+++ src/container/MainContainer.js
@@ -2,29 +2,44 @@ import React, {Component} from 'react'
 import Image from '../components/Image';
 import ColorPicker from '../components/ColorPicker';
 
+import html2canvas from 'html2canvas';
+
 class MainContainer extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            TieColor: 'blue'
+            TieColor: 'blue',
+            bcgScale: 1,
+            bcgRotate: 0,
+            tieX: 30,
+            tieY: 70,
+            imgRotate: 0,
+            imgX: 280,
+            imgY: 1000,
+            imgScale: 1
         };
         this.onChange = this.onChange.bind(this);
-        this.onClick = this.onClick.bind(this);
+        this.onColorPickerClick = this.onColorPickerClick.bind(this);
         this.onImageSet = this.onImageSet.bind(this);
         this.backgroundScale = this.backgroundScale.bind(this);
         this.backgroundRotate = this.backgroundRotate.bind(this);
+        this.setTieX = this.setTieX.bind(this);
+        this.setTieY = this.setTieY.bind(this);
+        this.imgRotate = this.imgRotate.bind(this);
+        this.imgX = this.imgX.bind(this);
+        this.imgY = this.imgY.bind(this);
+        this.imgScale = this.imgScale.bind(this);
+        this.saveTie = this.saveTie.bind(this);
     }
 
-    onClick = (e) => {
+    onColorPickerClick = (e) => {
         this.setState({
             TieColor: e.target.style.backgroundColor,
         });
     };
     onChange = ({target: {value}}) =>
         this.setState({
-            TieColor: value,
-            bcgScale: 0.5,
-            bcgRotate: 180
+            TieColor: value
         });
     onImageSet = (event) => {
         if (event.target.files && event.target.files[0]) {
@@ -32,6 +47,12 @@ class MainContainer extends Component {
                 image: URL.createObjectURL(event.target.files[0])
             });
         }
+        // let reader = new FileReader();
+        // reader.readAsDataURL(URL.createObjectURL(event.target.files[0]));
+        // reader.onloadend = function() {
+        //     base64data = reader.result;
+        //     console.log(base64data);
+        // }
     };
     backgroundScale = ({target: {value}}) => {
         this.setState({
@@ -43,6 +64,78 @@ class MainContainer extends Component {
             bcgRotate: value
         });
     };
+    setTieX = ({target: {value}}) => {
+        this.setState({
+            tieX: value
+        });
+    };
+    setTieY = ({target: {value}}) => {
+        this.setState({
+            tieY: value
+        });
+    };
+    imgRotate = ({target: {value}}) => {
+        this.setState({
+            imgRotate: parseInt(value)
+        });
+    };
+    imgX = ({target: {value}}) => {
+        this.setState({
+            imgX: parseInt(value)
+        });
+    };
+    imgY = ({target: {value}}) => {
+        this.setState({
+            imgY: parseInt(value)
+        });
+    };
+    imgScale = ({target: {value}}) => {
+        this.setState({
+            imgScale: parseFloat(value)
+        });
+    };
+    saveTie = () => {
+        let textToWrite = document.getElementById('image-wrapper').innerHTML;
+        let textFileAsBlob = new Blob([textToWrite], {type: 'image/svg+xml'});
+        let fileNameToSaveAs = "svg";
+        console.log(textToWrite);
+
+        let downloadLink = document.createElement("a");
+        downloadLink.download = fileNameToSaveAs;
+        downloadLink.innerHTML = "Download File";
+        if (window.URL != null) {
+            downloadLink.href = window.URL.createObjectURL(textFileAsBlob);
+        }
+        else {
+            downloadLink.href = window.URL.createObjectURL(textFileAsBlob);
+         //   downloadLink.onclick = destroyClickedElement;
+            downloadLink.style.display = "none";
+            document.body.appendChild(downloadLink);
+        }
+        downloadLink.click();
+        // this.generateScreenshot()
+    };
+
+    generateScreenshot() {
+        html2canvas(document.getElementById('tie'), {
+            width: 260,
+            height: 565,
+            scale: 10,
+            logging: true,
+            profile: true,
+            useCORS: true
+        }).then(function (canvas) {
+            //setDPI(canvas, 600);
+            console.log(canvas);
+            var data = canvas.toDataURL('image/png', 0.1);
+            // console.log(data);
+            var src = encodeURI(data);
+            // document.getElementById('screenshot').src = src;
+            document.getElementById('size').innerHTML = src.length + ' bytes';
+            document.getElementById('test').href = data;
+            document.getElementById('test').download = 'atkinsons_' + new Date().toLocaleString() + '.png';
+        });
+    }
 
     render() {
         let isImported = false;
@@ -52,22 +145,36 @@ class MainContainer extends Component {
         }
         const mainStyle = {
             display: 'flex',
-            flexDirection: 'row'
+            flexDirection: 'row',
+            justifyContent: 'space-around'
         };
         return (
             <div style={mainStyle}>
                 <Image color={this.state.TieColor}
                        image={this.state.image}
-                       bcgScale = {this.state.bcgScale}
-                       bcgRotate = {this.state.bcgRotate}
-
+                       bcgScale={this.state.bcgScale}
+                       bcgRotate={this.state.bcgRotate}
+                       tieX={this.state.tieX}
+                       tieY={this.state.tieY}
+                       imgRotate={this.state.imgRotate}
+                       imgScale={this.state.imgScale}
+                       imgX={this.state.imgX}
+                       imgY={this.state.imgY}
+                       saveTie={this.saveTie}
                 />
                 <ColorPicker TieColor={this.state.TieColor}
                              onImageSet={this.onImageSet}
-                             onClick={this.onClick}
+                             onClick={this.onColorPickerClick}
                              onChange={this.onChange}
                              getScale={this.backgroundScale}
-                             getRotate = {this.backgroundRotate}
+                             getRotate={this.backgroundRotate}
+                             getTieX={this.setTieX}
+                             getTieY={this.setTieY}
+                             imgRotate={this.imgRotate}
+                             imgX={this.imgX}
+                             imgY={this.imgY}
+                             imgScale={this.imgScale}
+
                              isImported={isImported}
                 />
             </div>
